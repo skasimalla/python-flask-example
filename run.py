0467e95b9b9ea6748fc67f51223e1f7fff78e121
Sam Kasimalla <45541473+skasimalla@users.noreply.github.com>
2023-06-16 15:09:23 -0400

    Update run.py

--- run.py
+++ run.py
@@ -10,3 +10,4 @@ import os
 # If file is called directly called, then run the app on the PORT provided defined in ENV or use '6969'.
 if __name__ == "__main__":
     app.run("0.0.0.0", port=os.getenv('PORT', 6969))
+    
